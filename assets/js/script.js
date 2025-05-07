e86da4f505881a97534f202a468aaee06563dc13
FazZenterprise <mpajri703@gmail.com>
2025-05-07 20:57:37 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frerlancer", "taruna", "anak kedua", "anak baik", "manusia"],
+    strings: ["freelancer", "taruna", "anak kedua", "anak baik", "manusia"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
